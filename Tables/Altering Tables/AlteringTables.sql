fbf0ca0cad12325feb962db67836a6560d3c36cb
caleblopez96 <caleblopez96@gmail.com>
2025-05-24 17:03:35 -0700

    ADDED: SQL script files for examples

---
+++ Tables/Altering Tables/AlteringTables.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE shop;
+
+USE shop;
+
+CREATE table customers(
+	id INT PRIMARY KEY auto_increment,
+	first_name varchar(50),
+	last_name varchar(50),
+	email varchar(50)
+);
+
+CREATE table orders(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	order_date DATE,
+  amount decimal(8,2),
+  customer_id INT,
+);
+
+ALTER TABLE orders
+ADD CONSTRAINT fk_customer_id
+FOREIGN KEY (customer_id) REFERENCES customers(id);
